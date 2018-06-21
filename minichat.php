aaa3bcb592bc16c8b0096f39f4ef58f25267b800
Sluca <luca008@hotmail.fr>
2018-06-21 13:12:24 +0200

    modif mini-chat design, ...

--- minichat.php
+++ minichat.php
@@ -1,120 +1,63 @@
 <?php
-
-
-// connexion à la base de donnée
-try {
-
-	$bdd = new PDO('mysql:host=localhost;dbname=tpminichat;charset=utf8', 'root', '',
-		array(
-      PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
-      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-      ));
-	
-	} catch (Execption $e) {
-
-	die('Erreur : '.$e->getMessage());
-}
+/*
+Script "mini_chat"
+    Par seb Luca
+Dernière modification : 26 01 2018
+*/
+	//connexion a la base de données
+	try
+	{
+		$bdd = new PDO('mysql:host=localhost;dbname=tp_coursphp;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+	}
+	catch(Exception $e)
+	{
+	    die('Erreur : '.$e->getMessage());
+	}
+?>
+<?php 
+	/*if(isset($_COOKIE['minichat_pseudo']))
+	{
+		echo $cookiePseudo = $_COOKIE['minichat_pseudo'];
+	}*/
 ?>
 <!doctype html>
 <html>
-<head>
-<meta charset="utf-8">
-<title>Mini chat</title>
+	<head>
+		<meta charset="utf-8">
+		<title>Mini-chat</title>
+		<link rel="stylesheet" type="text/css" href="css/styles.css">
+	</head>
+	<body>
+		<header>
+				<h1>Mini chat</h1>
+		</header>
+		<section>
+			<div id="content">
+				<div id="minichat">
+					<?php
+					$adresse_ip = $_SERVER['REMOTE_ADDR'];
 
-<link href="template.css" rel="stylesheet" type="text/css">
-</head>
-
-<body>
-<h1>TP Mini chat</h1>
-
-<div class="paragraphe"><p>Création minichat</p></div>
-<div class="container-fluid">
-<div class="form">
-<p>Ajouter un message</p>
-<form action="minichat_post.php" method="POST" enctype="multipart/form-data" />
-
-<fieldset>
-	<div class="form-group">
-	 	<label for="pseudo">Votre pseudo</label>
-	      <label for="pseudo">Pseudo :</label>
-<input type="text" name="pseudo" id="pseudo" value="<?php
- 
-  if(isset($_COOKIE['pseudo']))
-  {
-    echo htmlspecialchars($_COOKIE['pseudo']);
-  }
-   
-  ?>"><br>
-	</div>
-</fieldset>
-<fieldset>
-	<div class="form-group">
-	     <label for="message" title="Votre message">Votre message:</label>  
-	      <input name="message" type="text" id="message" />
-	</div>
-</fieldset>
-<button type="submit" name="submit value="Uploader" class="input-btn pull-left">Envoyer votre message</button>
- </p>
-    <!-- Recharger la page en appuyant sur le bouton -->
-    <p> <a href="javascript:window.location.reload()">Recharger la page</a> </p>
-</form>
-</div>
-<div class="news">
-<?php
-// On met dans une variable le nombre de messages qu'on veut par page
-$nombreDeMessagesParPage = 5; // Essayez de changer ce nombre pour voir :o)
-// On récupère le nombre total de messages
-$req = $bdd->query('SELECT COUNT(*) AS nb_message FROM chat');
-        $donnees = $req->fetch();
-
-$totalDesMessages = $donnees['nb_message'];
-// On calcule le nombre de pages à créer
-$nombreDePages  = ceil($totalDesMessages / $nombreDeMessagesParPage);
-// Puis on fait une boucle pour écrire les liens vers chacune des pages
- 
-if (isset($_GET['page']))
-{
-        $page = $_GET['page']; // On récupère le numéro de la page indiqué dans l'adresse
-}
-else // La variable n'existe pas, c'est la première fois qu'on charge la page
-{
-        $page = 1; // On se met sur la page 1 (par défaut)
-}
-  
-// On calcule le numéro du premier message qu'on prend pour le LIMIT de MySQL
-$premierMessageAafficher = ($page - 1) * $nombreDeMessagesParPage;
-
-$req = $bdd->prepare('SELECT id,pseudo,message,DATE_FORMAT(date_creation, \'%d/%m/%Y \') AS date_creation_fr FROM chat ORDER BY ID DESC LIMIT ' . $premierMessageAafficher . ', ' . $nombreDeMessagesParPage);
-	
-	$req->execute(array(
-		
-		));
-
-	while ($donnees = $req->fetch())
-		{
- ?>
-
-
-<p><?php echo htmlspecialchars($donnees['pseudo']); ?>
--
-<?php echo nl2br(htmlspecialchars($donnees['message'])); ?> - Posté le : <?php echo htmlspecialchars($donnees['date_creation_fr']); ?>
-</p>
-<?php
-}
-
-	$req->closeCursor(); // Termine le traitement de la requête
-echo '<ul class="pagination">
-<li><a href="minichat.php"> 1 </a></li>';
-
-for ($i = 2 ; $i <= $nombreDePages ; $i++)
-{
-   echo '<li><a href="minichat.php?page=' . $i . '">' . $i . '</a></li>
-    ';
-}
-
-echo '</ul>';
-?>
-</div>
-</div>
-</body>
+						$reponse = $bdd->query('SELECT * FROM chatsite ORDER BY minichat_id DESC LIMIT 0, 5');
+						while ($donnees = $reponse->fetch())
+						{
+							echo '<p><span class="pseudo">' .strtoupper(strip_tags($donnees['minichat_pseudo'])). '</span> - <span class="message">'.ucfirst(strip_tags($donnees['minichat_message'])).'</span></p>';
+						}
+					?>
+				</div>
+				
+				<?php header('Refresh: 10;URL=minichat.php'); ?>
+				<form action="minichat_post.php" method="POST" enctype="multipart/form-data">
+					<fieldset>
+						<p><input type="text" name="minichat_pseudo" id="pseudo" value="<?php if(isset($_COOKIE['minichat_pseudo'])) echo ucfirst($_COOKIE['minichat_pseudo']);?>" placeholder="Votre pseudo" /></p>
+						<p><textarea name="minichat_message" id="message" value="" placeholder="Votre message"></textarea></p>
+						<p><input id="upip" type="hidden" name="upip" value="<?php echo $adresse_ip; ?>" />
+						<p><button  type="submit" value="Envoyer">Envoyer</button></p>
+					</fieldset>
+				</form>
+			</div>
+	</section>
+		<footer>
+			<p id="footer"></p>
+		</footer>
+	</body>
 </html>
--- minichat_post.php
+++ minichat_post.php
@@ -1,48 +1,49 @@
 <?php
-setcookie('pseudo', $_POST['pseudo'], time() + 365*24*3600, null, null, false, true); // On écrit un cookie
-
-// connexion à la base de donnée
-try {
-
-	$bdd = new PDO('mysql:host=localhost;dbname=tpminichat;charset=utf8', 'root', '',
-		array(
-      PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
-      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-      ));
-	
-	} catch (Execption $e) {
-
-	die('Erreur : '.$e->getMessage());
-}
-
-
-if (isset($_POST['message']) AND !empty($_POST['message'])) { 
-
-
-	// On ajoute un message
-$req = $bdd->prepare('
-	INSERT INTO chat( pseudo, message, date_creation) 
-	VALUES (:pseudo, :message, NOW())');
-
-$req->execute(array(
-
-    
-    'pseudo' => $_POST['pseudo'],
-    'message' => $_POST['message'],
-    
-
-    ));
-
-header('Location: minichat.php');
-
-	}else{
-
-			
-			header('Location: minichat.php');
-
+/*
+Script "mini_chat"
+	Par seb Luca
+Dernière modification : 26 01 2018
+*/
+	try {
+		$bdd = new PDO('mysql:host=localhost;dbname=tp_coursphp;charset=utf8', 'root', 'root');
+	}
+	catch(Exception $e)
+	{
+		die('Erreur : '.$e->getMessage());
 	}
 
-
-
+	setcookie('minichat_pseudo',$_POST['minichat_pseudo'], time() + 365*24*3600, null, null, false, true); // On écrit un cookie
 ?>
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Post</title>
+	</head>
+	<body>
+		<?php
+			//print_r();
+			if(empty($_POST['minichat_pseudo']) OR empty($_POST['minichat_message']))
+			{
+				echo 'Le message n\'a pas été ajouté !';
+//time code php a intégrer
+//header('Location: minichat.php');
+
+			} else {
+				// Insertion du message à l'aide d'une requête préparée
+				$req = $bdd->prepare('INSERT INTO chatsite (minichat_pseudo, up_ip, minichat_message) VALUES(?, ?, ?)');
+				$req->execute(array(
+				$_POST['minichat_pseudo'],
+				$_POST['upip'],
+				$_POST['minichat_message']
+				));
+				echo 'Le message a bien été ajouté !';
+				
+//time code php a intégrer
+				// Puis rediriger
+				header('Location: minichat.php');
+			}
+		?>
+	</body>
+</html>
 
